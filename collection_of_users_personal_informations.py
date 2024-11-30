de3c756ea615a51ef1c0ba903f8c8058d74a563c
LianKarylGarcia <liankarylg@gmail.com>
2024-11-30 01:04:06 -0500

    fixed the .append function

--- collection_of_users_personal_informations.py
+++ collection_of_users_personal_informations.py
@@ -27,58 +27,43 @@ while True: # First loop
     try:
         print("Greetings, please answer the following questions about yourself.")
         
-        full_name = input("What is your full name? ")
+        full_name.append = input("What is your full name? ")
         print(f"Hi {full_name}!")
-        full_name.append(full_name)
-
-        birthday = input("When is your birthday? ")
-        birthday.append(birthday)
-        
-        age = int(input("How old are you? "))
+       
+        birthday.append = input("When is your birthday? ")
+       
+        age.append = int(input("How old are you? "))
         if age >= 18:
             print("You are an adult")
         else:
             print("You are a minor")
-        age.append(age)
 
-        birthplace = input("Where were you born? ")
-        birthplace.append(birthplace)
+        birthplace.append = input("Where were you born? ")
+       
+        address.append = input("Where do you live? ")
 
-        address = input("Where do you live? ")
-        address.append(address)
+        mothers_name.append = input("What is your mother's name? ")
 
-        mothers_name = input("What is your mother's name? ")
-        mothers_name.append(mothers_name)
+        fathers_name.append = input("What is your father's name? ")
+  
+        school.append = input("Where do you study? ")
 
-        fathers_name = input("What is your father's name? ")
-        fathers_name.append(fathers_name)
+        year.append = input("What year are you? ")
+       
+        course.append = input("What is your course? ")
+    
+        favorite_movie.append = input("What is your favorite movie? ")
 
-        school = input("Where do you study? ")
-        school.append(school)
+        favorite_color.append = input("What is your favorite color? ")
+      
+        favorite_artist.append = input("Who is your favorite artist? ")
 
-
-        year = input("What year are you? ")
-        year.append(year)
-
-        course = input("What is your course? ")
-        course.append(course)
-
-        favorite_movie = input("What is your favorite movie? ")
-        favorite_movie.append(favorite_movie)
-
-        favorite_color = input("What is your favorite color? ")
-        favorite_color.append(favorite_color)
-
-        favorite_artist = input("Who is your favorite artist? ")
-        favorite_artist.append(favorite_artist)
-
-        favorite_song = input("What is your favorite song? ")
-        favorite_song.append(favorite_song)
+        favorite_song.append = input("What is your favorite song? ")
 
         print("Wow! You have good taste!")
 
         print(f"Thank you for answering, {full_name}!")
-        
+
     except: 
         print("Invalid Input")
 
