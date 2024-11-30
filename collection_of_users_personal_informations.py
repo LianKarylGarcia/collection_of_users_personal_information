d12930f8878d964679df086da89a9509612ad8a9
LianKarylGarcia <liankarylg@gmail.com>
2024-11-29 23:01:57 -0500

    Pseudocode, and list of informations

---
+++ collection_of_users_personal_informations.py
@@ -0,0 +1,23 @@
+# Put information in an array
+# Use a while loop so it doesn't end if the input is invalid.
+# Ask the user for inputs.
+# Store it in a text file.
+
+# Personal Information
+full_name = []
+birthday = []
+birthplace = []
+address = []
+mothers_name = []
+fathers_name = []
+
+# Education
+school = []
+year = []
+course = []
+
+# Preferences
+favorite_movie = []
+favorite_color = []
+favorite_artist = []
+favorite_song = []
