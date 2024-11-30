7ed1e4d22d0513e45594ba29d2362bc94c95c987
LianKarylGarcia <liankarylg@gmail.com>
2024-11-30 00:54:38 -0500

    Set some conditions, and used .append to add new inputs

--- collection_of_users_personal_informations.py
+++ collection_of_users_personal_informations.py
@@ -27,23 +27,58 @@ while True: # First loop
     try:
         print("Greetings, please answer the following questions about yourself.")
         
-        print("Please follow this format: Surname, First Name Middle Initial")
         full_name = input("What is your full name? ")
+        print(f"Hi {full_name}!")
+        full_name.append(full_name)
 
         birthday = input("When is your birthday? ")
+        birthday.append(birthday)
+        
         age = int(input("How old are you? "))
-        birthplace = input("When were you born? ")
+        if age >= 18:
+            print("You are an adult")
+        else:
+            print("You are a minor")
+        age.append(age)
+
+        birthplace = input("Where were you born? ")
+        birthplace.append(birthplace)
+
         address = input("Where do you live? ")
+        address.append(address)
+
         mothers_name = input("What is your mother's name? ")
+        mothers_name.append(mothers_name)
+
         fathers_name = input("What is your father's name? ")
+        fathers_name.append(fathers_name)
 
         school = input("Where do you study? ")
+        school.append(school)
+
+
         year = input("What year are you? ")
+        year.append(year)
+
         course = input("What is your course? ")
+        course.append(course)
 
         favorite_movie = input("What is your favorite movie? ")
+        favorite_movie.append(favorite_movie)
+
         favorite_color = input("What is your favorite color? ")
+        favorite_color.append(favorite_color)
+
         favorite_artist = input("Who is your favorite artist? ")
+        favorite_artist.append(favorite_artist)
+
         favorite_song = input("What is your favorite song? ")
+        favorite_song.append(favorite_song)
+
+        print("Wow! You have good taste!")
+
+        print(f"Thank you for answering, {full_name}!")
+        
     except: 
         print("Invalid Input")
+
